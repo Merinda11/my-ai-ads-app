12b53325c30b413a1343686f92032a454ab9f5b2
Merin <merin@example.com>
2025-03-31 18:37:35 +0700

    Fix package.json error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import { signIn } from "next-auth/react";
 
 export default async function HomePage() {
